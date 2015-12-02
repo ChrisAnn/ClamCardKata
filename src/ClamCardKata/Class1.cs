c1d6f10a71744eab265f9655b1697243d189d9b6
Chris O'Dell <christine.odell@just-eat.com>
2015-12-02 22:57:39 +0000

    Renamed to ClamCardKata to prevent confusion

---
+++ src/ClamCardKata/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ClamCardKata
+{
+    [TestFixture]
+    public class ClamCardTests
+    {
+        [Test]
+        public void Single_Journey_In_Zone_A_Correctly_Charged()
+        {
+        }
+    }
+}
