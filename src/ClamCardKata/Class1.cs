ae31a1c3af274da2dcf62a2c9db0a21174a7521b
Chris O'Dell <christine.odell@just-eat.com>
2015-12-02 23:08:44 +0000

    Added Test for a single journey in the B zone

--- src/ClamCardKata/Class1.cs
+++ src/ClamCardKata/Class1.cs
@@ -14,18 +14,25 @@ namespace ClamCardKata
             Assert.That(card.Total, Is.EqualTo(2.5));
         }
 
+        [Test]
+        public void Single_Journey_In_Zone_B_Correctly_Charged()
+        {
+            var card = new ClamCard();
+            card.AddJourney("Barbican", "Balham");
+
+            Assert.That(card.Total, Is.EqualTo(3));
+        }
     }
 
     public class ClamCard
     {
-        public void AddJourney(string FromStation, string ToStation)
-        {
-            
-        }
+        public double Total;
 
-        public double Total()
+        public void AddJourney(string originStation, string destinationStation)
         {
-            return 2.5;
+            if (originStation == "Asterisk" && destinationStation == "Aldgate")
+                Total = 2.5;
+            else Total = 3;
         }
     }
 }
